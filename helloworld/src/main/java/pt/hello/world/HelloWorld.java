f945fa7fa64212ca542c50f5e0f515e5bc429fc5
Chris Zaremba <ptrtonull@gmail.com>
2022-10-16 17:38:53 +0200

    Chris's test2

--- helloworld/src/main/java/pt/hello/world/HelloWorld.java
+++ helloworld/src/main/java/pt/hello/world/HelloWorld.java
@@ -1,7 +1,7 @@
 package pt.hello.world;
 
 public class HelloWorld {
-
+	// Another conflict test :)
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Witaj świecie!");
