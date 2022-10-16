f233bfaa3abd0c6c087c74ccb73930104868123e
Aleksandra Zaremba <aleksandra.zaremba02@gmail.com>
2022-10-16 17:37:31 +0200

    Add new file

---
+++ helloworld/src/main/java/pt/hello/world/HelloWorld.java
@@ -0,0 +1,10 @@
+package pt.hello.world;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Witaj świecie!");
+	}
+
+}
