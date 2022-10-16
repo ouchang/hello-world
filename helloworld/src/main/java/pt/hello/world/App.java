de2a98e0e348b44d35d6478d058985cb84ee2676
Aleksandra Zaremba <aleksandra.zaremba02@gmail.com>
2022-10-16 17:17:04 +0200

    Maven initial structure

---
+++ helloworld/src/main/java/pt/hello/world/App.java
@@ -0,0 +1,13 @@
+package pt.hello.world;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World! My name is Ola!" );
+    }
+}
